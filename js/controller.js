070c9fa3ec76ed80408627886fdb8d9630262f1c
zhpjy <zhpjy@qq.com>
2018-06-10 01:56:42 +0800

    init

---
+++ js/controller.js
@@ -0,0 +1,157 @@
+window.onload = function () {
+    var btnOnlineExercise = document.getElementById("btnOnlineExercise");
+    btnOnlineExercise.onclick = function () {
+        return clickGenerateExercise();
+    }
+}
+
+function downloadExercise() {
+    try {
+        data = {
+            content: [{
+                columns: [
+                    {text: ""},
+                    {text: ""},
+                    {text: ""}
+                ]
+            }],
+            style: {}
+        };
+        var list = exercise.operationList;
+        for (let i = 0, j = 0; i < list.length; i++, j++) {
+            let item = list[i];
+            data.content[0].columns[j].text += item.leftOperand + " " + item.operator + " " + item.rightOperand + " = \n";
+            if (j == 2) {
+                j = -1;
+            }
+        }
+        pdfMake.createPdf(data).download("questions");
+    } catch (error) {
+        console.log(error.stack);
+    }
+}
+
+function clickGenerateExercise() {
+    var questionNumMin = parseInt(document.getElementById("question-num-min").value);
+    var questionNumMax = parseInt(document.getElementById("question-num-max").value);
+    var answerNumMin = parseInt(document.getElementById("answer-num-min").value);
+    var answerNumMax = parseInt(document.getElementById("answer-num-max").value);
+    var questionCount = parseInt(document.getElementById("question-count").value);
+    var subBtnContainer = document.getElementById("subbutton-container");
+    var settings = document.getElementById("exercise-settings");
+    var questions = document.getElementById("questions");
+    //表驱动
+    var functionMap = new Map([
+        ['all-add', 'genAllAddExercise'],
+        ['all-sub', 'genAllSubExercise'],
+        ['add-and-sub', 'genMixExercise']
+    ]);
+    var radioButtons = document.getElementsByClassName("type-select");
+    var radioButtonValue;
+
+    for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked == true) {
+            radioButtonValue = radioButtons[i].value;
+        }
+    }
+
+    try {
+        checkInput();
+        eval(functionMap.get(radioButtonValue) + "(" + questionCount + ")");
+        exercise.operandUpper = questionNumMax;
+        exercise.operandLower = questionNumMin;
+        exercise.valueUpper = answerNumMax;
+        exercise.valueLower = answerNumMin;
+        exercise.generateExercise();
+
+        settings.hidden = true;
+        exercise.present(questions);
+        subBtnContainer.innerHTML += "<button class=\"btn btn-primary btn-lg btn-block\" type=\"button\" id=\"btn-sub-exercise\">提交试题</button>";
+
+        var btnSubExercise = document.getElementById("btn-sub-exercise");
+        var btnDownload = document.getElementById("btn-download");
+        btnSubExercise.onclick = function () {
+            return clickCheckExercise();
+        }
+        btnDownload.style.display = "block";
+        btnDownload.onclick = function () {
+            return downloadExercise();
+        }
+    } catch (error) {
+        alert(error);
+    }
+
+}
+
+function clickCheckExercise() {
+    var countRight = 0;
+    var countFault = 0;
+    var questions = document.getElementById("questions").children;
+    var alertsContainner = document.getElementById("alerts-container");
+
+    try {
+        checkInput();
+        for (let i = 0; i < questions.length; i++) {
+            let item = questions[i];
+            let rawAnswer = item.getElementsByTagName("input")[0].value;
+            let answer = exercise.operationList[i].value;
+            if (rawAnswer == answer) {
+                countRight++;
+                item.getElementsByTagName("span")[0].innerHTML = "✓";
+                item.getElementsByTagName("span")[0].style.color = "limegreen";
+                item.getElementsByTagName("span")[0].style.visibility = "visible";
+            } else {
+                countFault++;
+                item.getElementsByTagName("span")[0].innerHTML = "✗";
+                item.getElementsByTagName("span")[0].style.color = "red";
+                item.getElementsByTagName("span")[0].style.visibility = "visible";
+
+            }
+        }
+        if (countFault == 0) {
+            alertsContainner.innerHTML += "        <div class=\"alert alert-success\" role=\"alert\">\n" +
+                "            全对！\n" +
+                "        </div>";
+        } else {
+            alertsContainner.innerHTML += "        <div class=\"alert alert-primary\" role=\"alert\">\n" +
+                "            做对了 <strong>" + countRight + "</strong> 题，做错了 <strong>" + countFault + "<strong> 题。\n" +
+                "        </div>";
+        }
+    } catch (error) {
+        alert(error);
+    }
+}
+
+function genMixExercise(count) {
+    exercise = new Exercise(count);
+    return exercise;
+}
+
+function genAllAddExercise(count) {
+    exercise = new AddExercise(count);
+    return exercise;
+}
+
+function genAllSubExercise(count) {
+    exercise = new SubExercise(count);
+    return exercise;
+}
+
+function isNumber(str) {
+    if (str === "") {
+        throw new Error("输入不能为空！");
+    } else if (isNaN(str)) {
+        console.log(str);
+        throw new Error("输入的内容只能是数字。");
+    }
+}
+
+function checkInput() {
+    var inputs = document.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].getAttribute("type") === "text") {
+            let str = inputs[i].value;
+            isNumber(str);
+        }
+    }
+}
---
+++ js/genQuestion.js
@@ -0,0 +1,213 @@
+//算式类
+class BinaryOperation {
+    constructor() {
+        this.operandUpper = 100;
+        this.operandLower = 0;
+        this.valueUpper = 100;
+        this.valueLower = 0;
+        this.leftOperand = 0;
+        this.rightOperand = 0;
+        this.operator = "+";
+        this.value = 0;
+    }
+
+    calculate() {
+        console.log("override this function, function name calculate");
+    }
+
+    checkingCalculation() {
+        if (this.value <= this.valueUpper && this.value >= this.valueLower) {
+            return true;
+        }
+        return false;
+    }
+
+    equals() {
+        console.log("override this function, function name equals");
+    }
+
+    generateRandom() {
+        let num = Math.floor((Math.random() * (this.operandUpper - this.operandLower + 1) + this.operandLower));
+        return num;
+    }
+
+    generateBinaryOperation() {
+        let failureCount = 0;
+        while (true) {
+            this.leftOperand = this.generateRandom();
+            this.rightOperand = this.generateRandom();
+            this.calculate();
+            if (this.checkingCalculation() == true) {
+                return this;
+            } else {
+                failureCount++;
+                if (failureCount > 100) {
+                    throw new Error("数字范围设置错误");
+                }
+            }
+        }
+    }
+
+    present(content, num) {
+        content.innerHTML += "        <div class=\"question col-md-4\">\n" +
+            "            <label for=\"question" + num + "\" class=\"col-4 text-right\">" + this.leftOperand + this.operator + this.rightOperand + "=" + "</label>\n" +
+            "            <input type=\"text\" class=\"col-4 text-left\" id=\"question" + num + "\">\n" +
+            "            <span class=\"col-1\">✓</span>\n" +
+            "        </div>"
+    }
+}
+
+//加法算式类
+class AddBinaryOperation extends BinaryOperation {
+    constructor() {
+        super();
+        this.operator = "+";
+        // this.generateBinaryOperation();
+    }
+
+    calculate() {
+        this.value = this.leftOperand + this.rightOperand;
+    }
+
+    equals(bo) {
+        if (this.operator != bo.operator) {
+            return false
+        }
+        if ((this.leftOperand != bo.leftOperand && this.leftOperand != this.rightOperand) ||
+            this.rightOperand != bo.leftOperand && this.rightOperand != bo.rightOperand) {
+            return false;
+        }
+        return true;
+    }
+}
+
+//减法算式类
+class SubBinaryOperation extends BinaryOperation {
+    constructor() {
+        super();
+        this.operator = "-";
+        // this.generateBinaryOperation();
+    }
+
+    calculate() {
+        this.value = this.leftOperand - this.rightOperand;
+    }
+
+    equals(bo) {
+        if (this.operator != bo.operator) {
+            return false
+        }
+        if ((this.leftOperand != bo.leftOperand) || (this.rightOperand != bo.rightOperand)) {
+            return false;
+        }
+        return true;
+    }
+}
+
+//练习类
+class Exercise {
+    constructor(num) {
+        this.operandUpper = 100;
+        this.operandLower = 0;
+        this.valueUpper = 100;
+        this.valueLower = 0;
+        this.operationNumber = num;
+        this.operationList = new Array();
+    }
+
+    //产生一个随机的算式对象
+    generateBinaryExercise() {
+        let operation;
+        if (Math.round(Math.random()) == 1) {
+            operation = new AddBinaryOperation();
+        } else {
+            operation = new SubBinaryOperation();
+        }
+        operation.operandUpper = this.operandUpper;
+        operation.operandLower = this.operandLower;
+        operation.valueLower = this.valueLower;
+        operation.valueUpper = this.valueUpper;
+        operation.generateBinaryOperation();
+        return operation;
+    }
+
+    //将算式对象添加到算式对象数组
+    generateExercise() {
+        let question = this.generateBinaryExercise();
+        this.operationList.push(question);
+        let fail = 0;
+        for (let i = 1; i < this.operationNumber;) {
+            let question = this.generateBinaryExercise();
+            if (this.contains(question) == false) {
+                i++;
+                this.operationList.push(question);
+            } else {
+                fail++;
+                //失败次数过多退出
+                if (fail > 100) {
+                    throw new Error("产生的试题过多，或者范围过小！");
+                }
+            }
+        }
+    }
+
+    //检查算式是否重复
+    contains(anOperation) {
+        for (let i = 0; i < this.operationList.length; i++) {
+            var tmp = this.operationList[i];
+            if (anOperation.equals(tmp)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //展现
+    present(content) {
+        for (let i = 0; i < this.operationList.length; i++) {
+            this.operationList[i].present(content, i);
+        }
+    }
+
+    debugPresent() {
+        for (let i = 0; i < this.operationList.length; i++) {
+            console.log(this.operationList[i].leftOperand + this.operationList[i].operator + this.operationList[i].rightOperand + "=" + this.operationList[i].value);
+        }
+    }
+}
+
+//纯加法练习类
+class AddExercise extends Exercise {
+    constructor(num) {
+        super(num);
+    }
+
+    generateBinaryExercise() {
+        let operation;
+        operation = new AddBinaryOperation();
+        operation.operandUpper = this.operandUpper;
+        operation.operandLower = this.operandLower;
+        operation.valueLower = this.valueLower;
+        operation.valueUpper = this.valueUpper;
+        operation.generateBinaryOperation();
+        return operation;
+    }
+}
+
+//纯减法练习类
+class SubExercise extends Exercise {
+    constructor(num) {
+        super(num);
+    }
+
+    generateBinaryExercise() {
+        let operation;
+        operation = new SubBinaryOperation();
+        operation.operandUpper = this.operandUpper;
+        operation.operandLower = this.operandLower;
+        operation.valueLower = this.valueLower;
+        operation.valueUpper = this.valueUpper;
+        operation.generateBinaryOperation();
+        return operation;
+    }
+}
